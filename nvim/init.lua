dac7f8214b5d182e25db8c1c99c6c38e7e1d6033
Alec Lawrence <150299829+Heromercer@users.noreply.github.com>
2025-04-17 09:49:16 -0400

    Change config to modified nvchad

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,37 @@
-require("core.options")
-require("core.keymaps")
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+vim.g.mapleader = " "
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
-end ---@diagnostic disable-next-line: undefined-field
+
+if not vim.uv.fs_stat(lazypath) then
+    local repo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+end
+
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
+local lazy_config = require("configs.lazy")
+
+-- load plugins
 require("lazy").setup({
-	require("plugins.neotree"),
-	require("plugins.colortheme"),
-	require("plugins.bufferline"),
-	require("plugins.lualine"),
-	require("plugins.treesitter"),
-	require("plugins.telescope"),
-	require("plugins.lsp"),
-	require("plugins.autocompletion"),
-	require("plugins.autoformatting"),
-	require("plugins.gitsigns"),
-	require("plugins.alpha"),
-	require("plugins.indent-blankline"),
-	require("plugins.misc"),
-})
+    {
+        "NvChad/NvChad",
+        lazy = false,
+        branch = "v2.5",
+        import = "nvchad.plugins",
+    },
+
+    { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require("options")
+require("nvchad.autocmds")
+
+vim.schedule(function()
+    require("mappings")
+end)
---
+++ nvim/lua/options.lua
@@ -0,0 +1,14 @@
+require("nvchad.options")
+
+-- add yours here!
+
+local o = vim.o
+
+-- Indenting
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+
+-- Relative line number
+o.relativenumber = true
+-- o.cursorlineopt ='both' -- to enable cursorline!
